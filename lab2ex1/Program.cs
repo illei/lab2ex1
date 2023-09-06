9119f3fa066931ccfb4da7db50a1db815c9c2479
Illei Adrian <illei.adrian@yahoo.com>
2023-09-06 15:02:49 +0200

    Add project files.

---
+++ lab2ex1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab2ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+
+            int x = int.Parse(Console.ReadLine());
+            while (x != 0)
+            {
+
+
+                ;
+                Console.WriteLine("\n");
+
+
+                suma = suma + x % 10;
+
+                x = x / 10;
+
+
+            }
+
+
+            Console.WriteLine("suma este " + suma);
+        }
+    }
+}
